3bbae89be7de0c94155cca2fa62c40a856ec8fb1
Srinivas K <kundusrinivas006@gmail.com>
2025-09-20 16:33:54 +0530

    updated

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function Dashboard() {
   return (
     <View style={styles.container}>
-      <Text style={styles.message}>No trips completed yet</Text>
+      <Text style={styles.message}>No trips completed yet !!</Text>
     </View>
   );
 }
